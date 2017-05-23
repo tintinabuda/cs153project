a4bcdf48dee71aae6ef80b800586e75e1b7d7848
Tintin Abuda <kaz_abuda@yahoo.com>
2017-05-23 15:14:33 +0800

    Create migrations

---
+++ application/controllers/Migrate.php
@@ -0,0 +1,15 @@
+<?php
+class migrate extends CI_Controller {
+    public function index()
+    {
+        // load migration library
+        $this->load->library('migration');
+
+        if ( ! $this->migration->current())
+        {
+            echo 'Error' . $this->migration->error_string();
+        } else {
+            echo 'Migrations ran successfully!';
+        }
+    }
+}
---
+++ application/migrations/001_add_user.php
@@ -0,0 +1,42 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_Add_user extends CI_Migration {
+
+        public function up()
+        {
+                $this->dbforge->add_field(array(
+                        'id' => array(
+                                'type' => 'INT',
+                                'constraint' => 5,
+                                'unsigned' => TRUE,
+                                'auto_increment' => TRUE
+                        ),
+                        'username' => array(
+                                'type' => 'VARCHAR',
+                                'constraint' => '16',
+                        ),
+                        'name' => array(
+                                'type' => 'VARCHAR',
+                                'constraint' => '100',
+                        ),
+                        'birthday' => array(
+                                'type' => 'DATE',
+                        ),
+                        'address' => array(
+                                'type' => 'TEXT',
+                        ),
+                        'type' => array(
+                                'type' => 'INT',
+                        ),
+                ));
+                $this->dbforge->add_key('id', TRUE);
+                $this->dbforge->create_table('user');
+        }
+
+        public function down()
+        {
+                $this->dbforge->drop_table('user');
+        }
+}
